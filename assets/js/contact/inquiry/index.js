0faa0b5178d98b164b39c81b9d17dd09b890f666
柿本大志 <lilybell0412@gmail.com>
2020-12-21 13:58:09 +0900

    大サンプルと分割

--- assets/js/contact/inquiry/index.js
+++ assets/js/contact/inquiry/index.js
@@ -1,36 +1,25 @@
 (function ($) {
   var GA = window.GA || {};
 
-  GA.inquiry = function () {
-    var _init = function _init() {
-      $('.inquiry-check').change(function () {
-        var isChecked = $(this).prop('checked');
-        var $target = $('.js-toggleActive');
-
-        if (isChecked) {
-          $target.prop('disabled', false);
-        } else {
-          $target.prop('disabled', true);
-        }
-      });
-    };
-
+  GA.inquiry = (function () {
     $('.p-textform input[type="tel"],.p-textform input[type="email"],.inquiry-address input[type="text"]').change(function () {
       var textVal = $(this).val();
 
-      var replaseText = textVal.replace(/[Ａ-Ｚａ-ｚ０-９！＂＃＄％＆＇（）＊＋，－．／：；＜＝＞？＠［＼］＾＿｀｛｜｝]/g, function (s) {
-        return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
-      }).replace(/[‐－―]/g, '-') 
-      .replace(/[～〜]/g, '~') 
-      .replace(/　/g, ' '); 
+      var replaseText = textVal
+        .replace(/[Ａ-Ｚａ-ｚ０-９！＂＃＄％＆＇（）＊＋，－．／：；＜＝＞？＠［＼］＾＿｀｛｜｝]/g, function (s) {
+          return String.fromCharCode(s.charCodeAt(0) - 0xfee0);
+        })
+        .replace(/[‐－―]/g, "-")
+        .replace(/[～〜]/g, "~")
+        .replace(/　/g, " ");
 
       $(this).val(replaseText);
     });
 
     return {
-      init: _init
+      init: _init,
     };
-  }();
+  })();
 
   GA.inquiry.init();
 })(jQuery);
